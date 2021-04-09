59adb82dc1c7ef26412e83332af2e1fc201e7e99
Rishikagoud <82237178+Rishikagoud@users.noreply.github.com>
2021-04-10 01:09:03 +0530

    Created Size of data types.cpp
    This file prints the size of data types.

---
+++ Project Program - Size of data types.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+using namespace std;
+
+int main() 
+{
+	int i;
+	float f;
+	char c;
+	double d;
+	cout<<"Size of char :"<<sizeof(c)<<"byte"<<endl;
+	cout<<"Size of int :"<<sizeof(i)<<"bytes"<<endl;
+	cout<<"Size of float :"<<sizeof(f)<<"bytes"<<endl;
+	cout<<"Size of double :"<<sizeof(d)<<"bytes"<<endl;	
+	return 0;
+}
